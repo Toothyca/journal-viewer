b04f7715dd76725b705a16e4f9ddcf9dc5465ed0
Toothyca <Toothyca@gmail.com>
2023-07-04 17:08:49 -0700

    read jpg comments

--- journal_viewer.py
+++ journal_viewer.py
@@ -1,5 +1,8 @@
 import os
 import re
+from PIL import Image
+import PIL.ExifTags
+from PIL.ExifTags import TAGS
 import sqlite3
 
 def isYear(fileName):
@@ -13,6 +16,29 @@ def isYear(fileName):
         return True
     return False
 
-years = [file for file in os.listdir('aryday') if isYear(file)]
+path = "aryday"
+years = [os.path.join(path, file) for file in os.listdir(path) if isYear(file)]
 for yearFolder in years:
-    pass
+    #days = [file.strip(".jpg") for file in os.listdir(yearFolder) if file.endswith(".jpg")]
+    #print(days)
+    days = list()
+    content = list()
+    albums = list()
+    for day_name in os.listdir(yearFolder):
+        day_path = os.path.join(yearFolder, day_name)
+        if day_path.endswith(".jpg"):
+            days.append(day_name)
+            image = Image.open(day_path)
+            try:
+                #print(image.info.keys())
+                exif = {
+                    PIL.ExifTags.TAGS[k]: v
+                    for k, v in image._getexif().items()
+                    if k in PIL.ExifTags.TAGS
+                }
+                print(exif["XPComment"].decode())
+            except Exception as e:
+                print(e)
+        break
+    #print(days)
+        
